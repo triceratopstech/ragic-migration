1f474ccddf8c77b155ddfe4f35aae5356afc6ee1
Trevor Blanarik <tblanarik@github.com>
2025-03-02 07:56:40 +0000

    printing some sheets, ooh yeah

--- src/run.py
+++ src/run.py
@@ -3,13 +3,17 @@ import requests
 
 config = None
 
-def get_children():
+def get_tabs():
     url = f"https://{config['SERVER_URL']}/{config['ACCOUNT_NAME']}"
     data = requests.get(url, headers={"Authorization": "Basic " + config["API_KEY"]})
     j_data = data.json()[config['ACCOUNT_NAME']]
     return j_data["children"]
 
+def get_tab_sheets(tab):
+    return tab["children"]
+
 if __name__ == "__main__":
     with open("ragic.config", "r") as f:
         config = json.load(f)
-    print(get_children())
+    tabs = get_tabs()
+    print(get_tab_sheets(list(tabs.values())[2]))
